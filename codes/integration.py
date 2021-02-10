603abcc9852540e44400c23d006eb4bdfd6e624d
Zhiqi Huang <huangzhq25@mail.sysu.edu.cn>
2021-02-10 17:49:17 +0800

    update GR

--- codes/integration.py
+++ codes/integration.py
@@ -2,6 +2,7 @@
 import sympy as sym
 x = sym.Symbol('x')
 y = sym.Symbol('y')
-print(sym.integrate(sym.log(x),x))
-print(sym.integrate(sym.exp(-x**2)*sym.erf(x),x))
-print(sym.integrate(sym.exp(-x**2),(x, -sym.oo, sym.oo)))
+#print(sym.integrate(sym.log(x),x))
+print(sym.integrate(sym.sinh(x)**2,x))
+#print(sym.integrate(sym.exp(-x**2)*sym.erf(x),x))
+#print(sym.integrate(sym.exp(-x**2),(x, -sym.oo, sym.oo)))
